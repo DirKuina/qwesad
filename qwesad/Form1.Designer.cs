8dd643884d2ca641e8b91e14d1740e683d70f910
DirKuina <kuinakos@yandex.ru>
2021-03-05 19:05:30 +0300

    2

--- qwesad/Form1.Designer.cs
+++ qwesad/Form1.Designer.cs
@@ -30,12 +30,182 @@ namespace qwesad
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 261);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(281, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(358, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(471, 333);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 36);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Rename";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(596, 334);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 35);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(427, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(187, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(426, 251);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(187, 27);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(104, 327);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 41);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(817, 153);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(64, 61);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(763, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(920, 55);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(226, 333);
+            this.treeView1.TabIndex = 12;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1200, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "A";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
--- qwesad/Form1.cs
+++ qwesad/Form1.cs
@@ -7,15 +7,165 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Windows.Input;
+using System.Windows;
 
 namespace qwesad
 {
     public partial class Form1 : Form
     {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+        private string currentFolderPath;
+        private string newPath;
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = false;
+            timer1.Interval = 3000;
+            timer1.Tick += timer1_Tick;
+
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            foreach (DriveInfo d in allDrives)
+            {
+                textBox1.Text += $"Drive {d.Name} Drive type: {d.DriveType}";
+                if (d.IsReady == true)
+                {
+                    textBox1.Text += $", DriveFormat : {d.DriveFormat} , DriveTotalSize : {d.TotalSize / 1024 / 1024 / 1024} GB , DriveFreeSpace : {d.AvailableFreeSpace / 1024 / 1024 / 1024} GB" + Environment.NewLine;
+                }
+                else
+                {
+                    textBox1.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Stop();
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Start();
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            foreach (DriveInfo d in allDrives)
+            {
+                if (d.IsReady == true)
+                {
+                    textBox2.Text += $"Drive {d.Name} DriveTotalSize : {d.TotalSize} Bytes , DriveFreeSpace : {d.AvailableFreeSpace} Bytes" + Environment.NewLine;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentFolderPath = label1.Text;
+            newPath = textBox4.Text;
+            try
+            {
+
+                File.Copy(currentFolderPath, newPath, true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удается переместить файл из-за исключения: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                currentFolderPath = label1.Text;
+                newPath = textBox4.Text;
+                File.Move(currentFolderPath, newPath);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            currentFolderPath = label1.Text;
+
+            try
+            {
+                File.Delete(currentFolderPath);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+
+
+            foreach (System.IO.DriveInfo myDrives in System.IO.DriveInfo.GetDrives())
+            {
+                TreeNode myDrivesNode = treeView1.Nodes.Add(myDrives.Name);
+
+                myDrivesNode.Nodes.Add("Expand123");
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode MyExistNode = e.Node;
+
+            MyExistNode.Nodes.Clear();
+
+            try
+            {
+                string mypath = MyExistNode.FullPath;
+
+
+                foreach (string myFolders in System.IO.Directory.GetDirectories(mypath))
+                {
+                    TreeNode FldrNode = MyExistNode.Nodes.Add(System.IO.Path.GetFileName(myFolders));
+
+                    FldrNode.Nodes.Add("Expand123");
+                }
+
+
+                foreach (string MyFiles in System.IO.Directory.GetFiles(mypath))
+                {
+
+                    TreeNode FLNode = MyExistNode.Nodes.Add(System.IO.Path.GetFileName(MyFiles));
+
+                }
+
+            }
+            catch (Exception FlErr)
+            {
+                MessageBox.Show(FlErr.ToString());
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode MySelectedNode = e.Node;
+
+            label1.Text = MySelectedNode.FullPath;
+        }
     }
 }
